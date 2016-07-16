3468c221f8a6d45682853d7a4a3c4db7835d72c5
wodewojueding <wodewojueding@yeah.net>
2016-07-16 23:40:38 +0800

    商品维护，修改删除添加功能

---
+++ BL.Service/GoodsService.cs
@@ -0,0 +1,75 @@
+﻿using BL.Framework.Orm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BL.Models;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BL.Service
+{
+    public class GoodsService : DBContext
+    {
+        public IEnumerable<T_GOODSModel> GetAllGoodsInfo()
+        {
+            string sql = "select * from T_GOODS";
+            using (IDbConnection db = OpenConnection())
+            {
+                return db.Query<T_GOODSModel>(sql);
+            }
+        }
+
+        public int AddGoods(T_GOODSModel model)
+        {
+
+            string sql = @"insert into  T_GOODS(FGUID, FCREATEID, FCREATETIME, FID, FNAME, FSTANDARD, FUNIT, FCALCTYPE, FCATEGORY, FISCONSUMABLES, FSTATUS, FSTARTTIME, FENDTIME, FMEMO
+) values(@FGUID, @FCREATEID, @FCREATETIME, @FID, @FNAME, @FSTANDARD, @FUNIT, @FCALCTYPE, @FCATEGORY, @FISCONSUMABLES, @FSTATUS, @FSTARTTIME, @FENDTIME, @FMEMO
+)";
+            using (IDbConnection db = OpenConnection())
+            {
+               // SqlParameter[] parames={
+               //     new SqlParameter("FGUID",model.FGUID),
+               //     new SqlParameter("FCREATEID",model.FCREATEID),
+               //     new SqlParameter("FCREATETIME",model.FCREATETIME),
+               //     new SqlParameter("FID",model.FID),
+               //     new SqlParameter("FNAME",model.FNAME),
+               //     new SqlParameter("FSTANDARD",model.FSTANDARD),
+               //     new SqlParameter("FUNIT",model.FUNIT),
+               //     new SqlParameter("FCALCTYPE",model.FCALCTYPE),
+               //     new SqlParameter("FCATEGORY",model.FCATEGORY),
+               //     new SqlParameter("FISCONSUMABLES",model.FISCONSUMABLES),
+               //     new SqlParameter("FSTATUS",model.FSTATUS),
+               //     new SqlParameter("FSTARTTIME",model.FSTARTTIME),
+               //     new SqlParameter("FENDTIME",model.FENDTIME),
+               //     new SqlParameter("FMEMO",model.FMEMO)
+               // };
+               //return Convert.ToInt32( db.(sql,parames));
+                return db.Execute(sql, model);
+            }
+        }
+
+        public int EditGoods(T_GOODSModel model)
+        {
+
+            string sql = @"update  T_GOODS set FCREATEID=@FCREATEID, FCREATETIME=@FCREATETIME, FID=@FID, FNAME=@FNAME, FSTANDARD=@FSTANDARD, FUNIT=@FUNIT, FCALCTYPE=@FCALCTYPE, FCATEGORY=@FCATEGORY, FISCONSUMABLES=@FISCONSUMABLES, FSTATUS=@FSTATUS, FSTARTTIME=@FSTARTTIME, FENDTIME=@FENDTIME, FMEMO=@FMEMO
+where FGUID=@FGUID ";
+            using (IDbConnection db = OpenConnection())
+            {
+                return db.Execute(sql, model);
+            }
+        }
+
+        public int DelGoods(string FGUID)
+        {
+
+            string sql = @"delete  T_GOODS 
+where FGUID=@FGUID ";
+            using (IDbConnection db = OpenConnection())
+            {
+                return db.Execute(sql, new { FGUID=FGUID});
+            }
+        }
+    }
+}
---
+++ BL.Web/Controllers/GOODSController.cs
@@ -0,0 +1,111 @@
+﻿using BL.Framework;
+using BL.Models;
+using BL.MVC;
+using BL.Service;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BL.Web.Controllers
+{
+    public class GOODSController : Controller
+    {
+        //
+        // GET: /GOODS/
+        GoodsService goodsService = new GoodsService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [JsonException]
+        public string GetAllGoodsJson()
+        {
+            var lst = goodsService.GetAllGoodsInfo();
+            return JsonHelper.Instance.Serialize(new { list = lst, pageSize = lst.Count() });
+        }
+        [JsonException]
+        public string EditGoods(string json)
+        {
+            JArray t = (JArray)JsonConvert.DeserializeObject(json);
+            T_GOODSModel model = new T_GOODSModel();
+            model.FCREATEID = t[0]["FCREATEID"].ToString();
+            model.FCREATETIME =DateTime.Now;
+            model.FID = t[0]["FID"].ToString();
+            model.FNAME = t[0]["FNAME"].ToString();
+            model.FSTANDARD = t[0]["FSTANDARD"].ToString();
+            model.FUNIT = t[0]["FUNIT"].ToString();
+            model.FCALCTYPE = t[0]["FCALCTYPE"].ToString();
+            model.FCATEGORY = t[0]["FCATEGORY"].ToString();
+            model.FISCONSUMABLES = t[0]["FISCONSUMABLES"].ToString();
+            model.FSTATUS = t[0]["FSTATUS"].ToString();
+            
+            model.FMEMO = t[0]["FMEMO"].ToString();
+            int rel = 0;
+            if (t[0]["addFlag"]!=null && Convert.ToBoolean(t[0]["addFlag"].ToString()))
+            {
+                if (model.FSTATUS == "2")
+                {
+                    model.FSTARTTIME = DateTime.Now;
+                    model.FENDTIME = null;
+                }
+                else if (model.FSTATUS == "3")
+                {
+                    model.FENDTIME = DateTime.Now;
+                    model.FSTARTTIME = null;
+                }
+                model.FGUID = Guid.NewGuid().ToString();
+                rel=goodsService.AddGoods(model);
+            }
+            else
+            {
+                if (model.FSTATUS == "2")
+                {
+                    model.FSTARTTIME = DateTime.Now;
+                    if (t[0]["FENDTIME"].ToString() != "")
+                    {
+                        model.FENDTIME = Convert.ToDateTime(t[0]["FENDTIME"].ToString());
+                    }
+                }
+                else if (model.FSTATUS == "3")
+                {
+                    if (t[0]["FSTARTTIME"].ToString() != "")
+                    {
+                        model.FSTARTTIME = Convert.ToDateTime(t[0]["FSTARTTIME"].ToString());
+                    }
+                    model.FENDTIME = DateTime.Now;
+                }
+                model.FGUID = t[0]["FGUID"].ToString();
+                rel=goodsService.EditGoods(model);
+            }
+            if (rel > 0)
+            {
+                return JsonHelper.Instance.Serialize(new { statusCode = "200", message = "保存成功" });
+            }
+            else
+            {
+                return JsonHelper.Instance.Serialize(new { statusCode = "300", message = "保存失败" });
+            }
+        }
+        [JsonException]
+        [HttpPost]
+        public string DelGoods(string json)
+        {
+            JArray t = (JArray)JsonConvert.DeserializeObject(json);
+            int rel = goodsService.DelGoods(t[0]["FGUID"].ToString());
+            if (rel > 0)
+            {
+                return JsonHelper.Instance.Serialize(new { statusCode = "200", message = "删除成功" });
+            }
+            else
+            {
+                return JsonHelper.Instance.Serialize(new { statusCode = "300", message = "删除失败" });
+            }
+        }
+
+    }
+}
