f69df40a09318094e49ab36656b4f7b57024a359
kuaigoumanzhu <307788836@qq.com>
2016-07-19 21:44:12 +0800

    商品回库部分代码

---
+++ BL.Service/GoodsBackService.cs
@@ -0,0 +1,23 @@
+﻿using BL.Framework.Orm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BL.Models;
+using Dapper;
+using System.Data;
+
+namespace BL.Service
+{
+    public class GoodsBackService: DBContext
+    {
+        public IEnumerable<T_GOODSBACKModel> GetAllGoodsBackInfo()
+        {
+            string sql = "select * from T_GOODSBACK";
+            using (IDbConnection db = OpenConnection())
+            {
+                return db.Query<T_GOODSBACKModel>(sql);
+            }
+        }
+    }
+}
--- BL.Web/Controllers/GoodsBackController.cs
+++ BL.Web/Controllers/GoodsBackController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BL.Framework;
+using BL.MVC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BL.Service;
 
 namespace BL.Web.Controllers
 {
@@ -11,9 +14,16 @@ namespace BL.Web.Controllers
     /// </summary>
     public class GoodsBackController : Controller
     {
-        public ActionResult Index()
+        GoodsBackService goodsBack = new GoodsBackService();
+        public ActionResult GoodsBackList()
         {
             return View();
+        }
+        [JsonException]
+        public string GetGoodsBackList()
+        {
+            var lst = goodsBack.GetAllGoodsBackInfo();
+            return JsonHelper.Instance.Serialize(new { list=lst,pageSize=lst.Count()});
         }
 
     }
