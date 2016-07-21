d1bb57591ec1db26f9b54cd5ef0e51c7258e89d1
wodewojueding <wodewojueding@yeah.net>
2016-07-21 22:32:11 +0800

    字典数据获取，js

--- BL.Web/Content/common.js
+++ BL.Web/Content/common.js
@@ -1,4 +1,22 @@
-﻿function LoadDictCategory(selectID, CategoryID)
+﻿//供应商查询下拉
+function LoadGYSCategory(selectID)
+{
+    LoadDictCategory(selectID, 'SupplierCategory');
+}
+//省份查询下拉
+function LoadProvince(selectID) {
+    LoadDictCategory(selectID, 'Province');
+}
+//商品查询下拉
+function LoadGoodsCategory(selectID) {
+    LoadDictCategory(selectID, 'GoodsCategory');
+}
+//计量单位查询下拉
+function LoadUnit(selectID) {
+    LoadDictCategory(selectID, 'Unit');
+}
+
+function LoadDictCategory(selectID, CategoryID)
 {
     var url = "/DATADICT/GetCategoryJSON";
     if (CategoryID != "" && CategoryID != undefined)
@@ -17,4 +35,37 @@
             $("#"+selectID).html(_html);
         }
     });
+}
+//供应商数据表格编辑添加下拉
+function LoadGYSDataForRowSel() {
+    return LoadDataForRowSel( 'SupplierCategory');
+}
+////省份数据表格编辑添加下拉
+function LoadProvinceDataForRowSel() {
+    return LoadDataForRowSel( 'Province');
+}
+//商品数据表格编辑添加下拉
+function LoadGoodsDataForRowSel() {
+    return LoadDataForRowSel( 'GoodsCategory');
+}
+//计量单位数据表格编辑添加下拉
+function LoadUnitDataForRowSel() {
+    return LoadDataForRowSel( 'Unit');
+}
+
+function LoadDataForRowSel(categoryId)
+{
+    var rel ;
+    var url = "/DATADICT/GetDataCategoryJson";
+    if (categoryId != "" && categoryId != undefined) {
+        url += "?FCATEGORY=" + categoryId;
+    }
+    $(this).bjuiajax("doAjax", {
+        url: url,
+        async: false,
+        callback: function (json) {
+            rel = json;
+        }
+    });
+    return rel;
 }
